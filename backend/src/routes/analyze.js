347d53abfd02818889ef8f3275dd5c90fe8621ca
kshithesh <sathri.kshithesh2006@gmail.com>
2026-06-02 13:50:40 +0530

    Fix: Hard resetting file extensions to lowercase js

--- backend/src/routes/analyze.js
+++ backend/src/routes/analyze.js
@@ -7,7 +7,7 @@ import {
   simplifyClause,
   analyzePDF,
   compareDocuments
-} from '../controllers/analyzeController.JS';
+} from '../controllers/analyzeController.js';
 
 const router = express.Router();
 
